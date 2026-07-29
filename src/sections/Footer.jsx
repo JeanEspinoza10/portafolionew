b5621842e101ff6c6adecbb3ae0b71e51699ec9a
jespinoza <jeanria10@gmail.com>
2026-07-29 17:06:09 -0500

    add seo and banner

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -3,12 +3,13 @@ import linkedin from "../assets/linkedin.svg";
 import "../styles/footer.css";
 
 export const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <>
       <section className="main-footer">
         <section className="section-footer">
           <section className="section-footer-content">
-            <p>© 2025 Jean Espinoza</p>
+            <p>© {currentYear} Jean Espinoza</p>
             <a
               href="https://www.linkedin.com/in/jcespinozacdev/"
               target="_blank"
